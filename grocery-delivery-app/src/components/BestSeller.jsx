d939285033965e6a3ec0c4dd9157995a0ee03cbe
Pallabi Biswas <pallabibiswas4002@gmail.com>
2025-07-24 17:48:52 +0530

    give some padding

--- grocery-delivery-app/src/components/BestSeller.jsx
+++ grocery-delivery-app/src/components/BestSeller.jsx
@@ -6,7 +6,7 @@ const BestSeller = () => {
     const {products} = useAppContext()
   return (
     <div className='mt-10 px-2'>
-       <p className='text-2xl md:text-3xl font-medium ml-2'>Best Sellers</p>
+       <p className='text-2xl md:text-3xl font-medium'>Best Sellers</p>
        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-x-4 gap-y-4 sm:gap-3 md:gap-6 lg:grid-cols-5
          mt-6'>
            {products.filter((product)=> product.inStock).slice(0,5).map((product,index)=>(
--- grocery-delivery-app/src/components/Categories.jsx
+++ grocery-delivery-app/src/components/Categories.jsx
@@ -7,7 +7,7 @@ const Categories = () => {
   const {navigate} = useAppContext()
   return (
     <div className='mt-10 px-2'>
-      <p className='text-2xl md:text-3xl font-medium ml-2'>Categories</p>
+      <p className='text-2xl md:text-3xl font-medium'>Categories</p>
       <div className='grid grid-cols-2 sm:grid-cols-3  md:grid-cols-5 lg:grid-cols-6
       xl:grid-cols-7 mt-6 gap-4'>
 
